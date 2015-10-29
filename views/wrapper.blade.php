7c2c39091a93bbaff6c2f70c8cb8857588473867
Phil Martinez <phil@philsquare.com>
2015-10-28 22:56:07 -0500

    label is optional

--- views/wrapper.blade.php
+++ views/wrapper.blade.php
@@ -1,6 +1,10 @@
 <div class="uk-form-row">
     <span class="errors">{{ $errors->first($name) }}</span>
-    <label for="{{ $name }}" class="uk-form-label">{{ $label }}</label>
+
+    @if(isset($label))
+        <label for="{{ $name }}" class="uk-form-label">{{ $label }}</label>
+    @endif
+    
     <div class="uk-form-controls">
         @include('partials.form.' . $type)
     </div>
