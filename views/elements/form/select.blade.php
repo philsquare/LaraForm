ba07bcc1edeb4f1fd6ca53bb6bd864181b3cff3c
Phil Martinez <phil@philsquare.com>
2015-12-08 22:02:05 -0700

    small bug fixes in form elements

--- views/elements/form/select.blade.php
+++ views/elements/form/select.blade.php
@@ -4,7 +4,7 @@
 
     <select name="{{ $field['name'] }}">
         @foreach($field['options'] as $id => $title)
-            @if(isset($field['value']) && $id == $value)
+            @if(isset($field['value']) && $id == $field['value'])
                 <option value="{{ $id }}" selected>{{ $title }}</option>
             @else
                 <option value="{{ $id }}" {{ old($field['name']) == $id ? 'selected' : '' }}>{{ $title }}</option>
